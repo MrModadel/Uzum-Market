d8133e1e8018a78dd11e65384cf2d49d2ba9e785
MrModadel <timanormu@gmail.com>
2023-09-21 14:52:04 +0500

    news

--- pages/registration/index.js
+++ pages/registration/index.js
@@ -7,23 +7,34 @@ form.onsubmit = (e) => {
    let fm = new FormData(e.target);
    fm.forEach((value, key) => {
       user[key] = value;
+      if (!valid_test[key].test(value)){
+         alert('Заполните поля правильно)!')
+         return
+      }
    });
-   if (user.firstname && user.lastname && user.email && user.password) {
-      request('/users?email=' + user.email, "get")
-         .then(res => {
-            if (!res.length)
-               request('/users', 'post', user)
-                  .then(async res => {
-                     if (res) {
-                        await request('/karzina', 'post', { arr: [] })
-                        await request('/likes', 'post', { arr: [] })
-                        location.assign("/pages/login/");
-                     }
-                  })
-            else
-               alert('Это почта уже используется!')
-         })
-   } else {
-      alert("Заполните все поля");
-   }
+   if (user.email.ma)
+      if (user.firstname && user.lastname && user.email && user.password) {
+         request('/users?email=' + user.email, "get")
+            .then(res => {
+               if (!res.length)
+                  request('/users', 'post', user)
+                     .then(async res => {
+                        if (res) {
+                           await request('/karzina', 'post', { arr: [] })
+                           await request('/likes', 'post', { arr: [] })
+                           location.assign("/pages/login/");
+                        }
+                     })
+               else
+                  alert('Это почта уже используется!')
+            })
+      } else {
+         alert("Заполните все поля");
+      }
 };
+let valid_test = {
+   email: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+   firstname: /^[a-zA-Z]+(?:-[a-zA-Z]+)*$/,
+   password: /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d).*$/,
+   lastname: /^[a-zA-Z]+(?:-[a-zA-Z]+)*$/,
+}
